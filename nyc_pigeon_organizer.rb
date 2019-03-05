568820858a7c395a2f7636ac6183df8f5276bd18
Cody Yantis <cyantis@gmail.com>
2019-03-05 01:35:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   data.each do |trait, quality|
-    binding.pry
+    quality.each do |aspect, name|
+      pigeon_hash[name] ||= pigeon_hash[name] = []
+      pigeon_hash[name] << aspect
+    end
   end
+  pigeon_hash
 end
